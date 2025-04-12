de4f3f8abb6fe9a2d33b8a3e46a1739acdcf22a6
gokulprasad33 <gokulprasad2k5@gmail.com>
2025-04-12 19:55:00 +0530

    Release 0.1

--- app/src/main/java/com/example/brainsyncapp/Navigation/Screens.kt
+++
@@ -1,9 +0,0 @@
-package com.example.brainsyncapp.Navigation
-
-sealed class Screens(val route:String){
-    object Homescreen :Screens("homescreen")
-    object Settingscreen :Screens("settingscreen")
-    object ThemeScreen :Screens("themescreen")
-    object AboutScreen :Screens("aboutscreen")
-
-}
